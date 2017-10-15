c577c08193bb5f4acfb291674b458978a9d515b2
Billiam Crashkopf <crashkopf@users.noreply.github.com>
2017-10-14 18:42:12 -0700

    Fixed non-working external multiplexer, at least for P0..P5

--- adc.c
+++ adc.c
@@ -6,10 +6,11 @@
 // set MUXA / MUXB pins to correct bank
 #define ADC_SETBANK(n) {PORTL = (PORTL & ~0xc0) | ((n) << 6);}
 
+#define MUX_MASK (_BV(PL6) | _BV(PL7))
 
 void adc_init() {
-	DDRL |= 0xc0;    // MUXA and MUXB are outputs
-	PORTL = (PORTL & ~0xc0) | (0 << 6);  // bank number for external MUX
+	DDRL |= MUX_MASK;    // MUXA and MUXB are outputs
+	PORTL &= ~MUX_MASK;  // bank number for external MUX
 
 	ADMUX = 0x40;    // AVCC is AREF
 	ADCSRA |= 0x80;  // enable ADC
@@ -20,6 +21,9 @@ void adc_init() {
 }
 
 unsigned adc_read(unsigned chan) {
+
+	// Set the external MUX
+	PORTL = (PORTL & ~MUX_MASK) | ((chan << 2) & MUX_MASK);
 
 	if (chan >= 8) {
 		ADCSRB |= 0x08;  // MUX high channels (8 - 15)
--- pressure.h
+++ pressure.h
@@ -8,10 +8,10 @@
 #define MPXV7025_TO_HPA(x) ((x * 50000) / 92070)
 
 enum pressure_channel {
-	P0 = (AP0 & ADCBANK0),
-	P1 = (AP1 & ADCBANK0),
-	P2 = (AP0 & ADCBANK1),
-	P3 = (AP1 & ADCBANK1),
-	P4 = (AP0 & ADCBANK2),
-	P5 = (AP1 & ADCBANK2)
+	P0 = (AP0 | ADCBANK0),
+	P1 = (AP0 | ADCBANK1),
+	P2 = (AP0 | ADCBANK2),
+	P3 = (AP0 | ADCBANK3),
+	P4 = (AP1 | ADCBANK0),
+	P5 = (AP1 | ADCBANK1)
 };
