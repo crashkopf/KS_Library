49678e4a9aceceda3bd776c2d8f3b0fedc4e65ba
Billiam Crashkopf <crashkopf@users.noreply.github.com>
2017-10-22 10:37:53 -0700

    Added timer1_source() function to set timer1 clock source.

--- timer1.c
+++ timer1.c
@@ -3,28 +3,24 @@
 
 #include "timer1.h"
 
-void timer1_init() {
+void timer1_init(void) {
 	// Clear ouput compare registers
 	OCR1A = 0;
 	OCR1B = 0;
 	OCR1C = 0;
-
-	// Turn on the overflow interrupt
-	//TIMSK1 = _BV(TOIE1);
 
 	TCCR1A =
 		_BV(WGM10) | // Phase correct, 10-bit PWM
 		_BV(WGM11) |
 		0;
 
-	TCCR1B =
-		_BV(CS10) |
-		//_BV(CS11) |
-		//_BV(CS12) |
-		0; // Divide by 1
+	TCCR1B = 0;
+	timer1_source(TIMER1_INT);
 
 	TCCR1C = 0;
 }
+
+void timer1_source(enum timer1_clock_source cs) { TCCR1B = (TCCR1B & 0xF8) | cs; };
 
 void OC1A_enable() { TCCR1A |= _BV(COM1A1); }
 void OC1A_disable() { TCCR1A &= ~(_BV(COM1A1)); }
@@ -32,11 +28,3 @@ void OC1B_enable() { TCCR1A |= _BV(COM1B1); }
 void OC1B_disable() { TCCR1A &= ~(_BV(COM1B1)); }
 void OC1C_enable() { TCCR1A |= _BV(COM1C1); }
 void OC1C_disable() { TCCR1A &= ~(_BV(COM1C1)); }
-
-ISR(TIMER1_OVF_vect, ISR_NAKED) {
-	// Don't do anything...
-	asm volatile (
-		"reti\n\t"
-		::
-	);
-}
--- timer1.h
+++ timer1.h
@@ -2,7 +2,19 @@
 extern "C" {
 #endif
 
+enum timer1_clock_source {
+	TIMER1_DISABLED = 0,
+	TIMER1_INT = 1,
+	TIMER1_INT_8 = 2,
+	TIMER1_INT_64 = 3,
+	TIMER1_INT_256 = 4,
+	TIMER1_INT_1024 = 5,
+	TIMER1_EXT_FALLING = 6,
+	TIMER1_EXT_RISING = 7
+};
+
 void timer1_init(void);
+void timer1_source(enum timer1_clock_source cs);
 
 void OC1A_enable(void);
 void OC1A_disable(void);
