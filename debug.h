5acf379506d89fe3819628b96f06706ca482c3d4
Billiam Crashkopf <crashkopf@users.noreply.github.com>
2017-12-04 10:04:05 -0800

    debug() macro now in KSLIB_NODEBUG conditional

--- debug.h
+++ debug.h
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <avr/pgmspace.h>
 
-#define debug(string, ...) fprintf_P(&debug, PSTR(string), ##__VA_ARGS__)
-
 #ifndef KSLIB_NODEBUG
+
+#define debug(string, ...) fprintf_P(&debug, PSTR(string), ##__VA_ARGS__)
 
 extern FILE debug;
 
